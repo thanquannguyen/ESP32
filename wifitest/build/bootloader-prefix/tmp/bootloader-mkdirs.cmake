b70e526d914d3ea76b43d4bd9857e384b9fb0db3
thanquannguyen <91314423+thanquannguyen@users.noreply.github.com>
2023-06-01 14:47:26 +0700

    wifi

---
+++ wifitest/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "D:/esp/esp-idf/components/bootloader/subproject"
+  "D:/ESP32LAB/wifitest/build/bootloader"
+  "D:/ESP32LAB/wifitest/build/bootloader-prefix"
+  "D:/ESP32LAB/wifitest/build/bootloader-prefix/tmp"
+  "D:/ESP32LAB/wifitest/build/bootloader-prefix/src/bootloader-stamp"
+  "D:/ESP32LAB/wifitest/build/bootloader-prefix/src"
+  "D:/ESP32LAB/wifitest/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "D:/ESP32LAB/wifitest/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "D:/ESP32LAB/wifitest/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
